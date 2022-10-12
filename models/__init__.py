e0ba970e250304d158ca962a05ed0b832f58cbaf
sheggz <emmanuelikoya99@gmail.com>
2022-10-12 23:55:47 +0000

    Switch Db engine to SQLAlchemy

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
+
+import os
+
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 
+if "HBNB_TYPE_STORAGE" in os.environ.keys() and \
+        os.getenv("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    name = Column(
+        String(128),
+        nullable=False,
+    )
+
+    cities = relationship("City", backref="state",
+                          cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        _cities = []
+
+        all_cities = models.storage.all(City)
+
+        for city_key in all_cities:
+            if all_cities[city_key].state_id == self.id:
+                _cities.append(all_cities[city_key].state_id)
+
+        return _cities
